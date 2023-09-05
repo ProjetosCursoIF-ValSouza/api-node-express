83108af2193f970016cf10cb6d69b94efecbac58
Valdemir <souza.valdemir@aluno.ifsp.edu.br>
2023-09-05 16:18:13 -0300

    alterações de praxe!

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -4,13 +4,11 @@ import logout from '../controllers/auth/logout.js'
 
 const router = express.Router()
 
-// router.get('/', getAuth
+
 
 router.post('/login', login)
 router.post('/logout', logout)
 
-// router.put('/', updateProduct)
 
-//router.delete('/', deleteProduct)
 
 export default router
