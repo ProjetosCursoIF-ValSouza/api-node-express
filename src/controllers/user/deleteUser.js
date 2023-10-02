7b8ee69e6e9aad63cb86ac057a47ddaa39cafd41
Valdemir <souza.valdemir@aluno.ifsp.edu.br>
2023-10-02 13:35:23 -0300

    novas mudanças de 02/10/2023

--- src/controllers/user/deleteUser.js
+++ src/controllers/user/deleteUser.js
@@ -1,24 +1,47 @@
-import user from "../../models/userModel.js"
+import user from "../../models/userModel.js"; // Importa o modelo de dados 'userModel.js' que lida com operações de usuário no banco de dados.
 
-const deleteUser = async(req, res) => {
-    try{
-        const userData = req.body
-        const [result] = await user.remove(userData.id)
-        if(result.affectedRows === 1){
+const deleteUser = async (req, res) => {
+    try {
+        const userData = req.body; // Obtém os dados da solicitação HTTP (o ID do usuário a ser excluído).
+        const [result] = await user.remove(userData.id); // Chama a função 'remove' do modelo de dados para excluir o usuário com base no ID fornecido.
+
+        if (result.affectedRows === 1) {
+            // Se a exclusão for bem-sucedida (afetou uma única linha no banco de dados), responde com um status 200 (OK) e uma mensagem de sucesso.
             res.json({
                 success: "Usuário Apagado com Sucesso!",
-            })
-        }else{
+            });
+        } else {
+            // Se nenhum usuário foi encontrado para exclusão, responde com um status 404 (Not Found) e uma mensagem de erro.
             res.status(404).json({
                 error: `Usuário id: ${userData.id} não Encontrado!`
-            })
+            });
         }
-    } catch (error){
-        console.log(error)
+    } catch (error) {
+        // Se ocorrer um erro durante o processamento, registra o erro no console e retorna uma resposta de status 500 (Internal Server Error).
+        console.log(error);
         res.status(500).json({
             error: "Erro no servidor!",
-        })
+        });
     }
 }
 
-export default deleteUser
+export default deleteUser; // Exporta a função 'deleteUser' para que ela possa ser usada em outros lugares do código.
+
+// Resumindo o funcionamento do arquivo 'deleteUser.js':
+
+// Importa o modelo de dados 'userModel.js' para interagir com o banco de dados.
+// Define uma função assíncrona chamada deleteUser que recebe os objetos req (representando a solicitação HTTP) e res (representando a resposta HTTP).
+// Obtém o ID do usuário a ser excluído da solicitação HTTP do corpo da solicitação (req.body).
+// Chama a função remove do modelo de dados para excluir o usuário com base no ID fornecido.
+// Verifica se a exclusão foi bem-sucedida (se afetou uma única linha no banco de dados).
+// Se a exclusão for bem-sucedida, responde com um status 200 (OK) e uma mensagem de sucesso.
+// Se nenhum usuário for encontrado para exclusão, responde com um status 404 (Not Found) e uma mensagem de erro.
+// Em caso de erro durante o processamento, captura exceções, registra o erro no console e responde com um status 500 (Internal Server Error) com uma mensagem
+// de erro genérica.
+// Esse controlador é responsável por excluir um usuário com base em seu ID no banco de dados e retornar a resposta apropriada para o cliente, seja um sucesso, 
+// um erro ou uma resposta 404 caso o usuário não seja encontrado.
+
+
+
+
+
--- src/database/api-web.sql
+++ src/database/api-web.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 28-Ago-2023 às 22:57
+-- Tempo de geração: 25-Set-2023 às 19:47
 -- Versão do servidor: 10.4.22-MariaDB
 -- versão do PHP: 8.0.13
 
@@ -20,8 +20,7 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `api-web`
 --
--- Definição do banco de dados `api-web`
-/*CREATE DATABASE IF NOT EXISTS `api-web` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci; */
+
 -- --------------------------------------------------------
 
 --
@@ -32,16 +31,19 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `name` varchar(200) NOT NULL,
   `email` varchar(500) NOT NULL,
-  `pass` varchar(256) NOT NULL
+  `pass` varchar(256) NOT NULL,
+  `photo` varchar(700) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Extraindo dados da tabela `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `pass`) VALUES
-(1, 'valdemir', 'souza.valdemir@aluno.ifsp.edu', '123123'),
-(2, 'João dos Santos', 'joao.santos@htmail.com', '123123');
+INSERT INTO `users` (`id`, `name`, `email`, `pass`, `photo`) VALUES
+(1, 'Renan C.', 'renanc@gmail.com', '312321', 'https://avatars.githubusercontent.com/u/4259630?v=4'),
+(2, 'Maria', 'maria@gmail.com', '123123', 'https://avatars.githubusercontent.com/u/104683974?v=4'),
+(4, 'Rafael', 'rafael@gmail.com', '123123', 'https://avatars.githubusercontent.com/u/110418519?v=4'),
+(5, 'Camila', 'camila@gmail.com', '123123', 'https://avatars.githubusercontent.com/u/141092604?v=4');
 
 --
 -- Índices para tabelas despejadas
@@ -61,52 +63,9 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de tabela `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-
-
-CREATE TABLE `product` (
-  `id` int(11) NOT NULL,
-  `name` varchar(30) NOT NULL,
-  `descricao` varchar(50) NOT NULL,
-  `preco` decimal(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Extraindo dados da tabela `users`
---
-
-INSERT INTO `product` (`id`, `name`, `descricao`, `preco`) VALUES
-(1, 'iphone_15', 'smartphone_nova_geracao', '1231.23'),
-(2, 'levit', 'auto_voador_embraer', '192231.23');
-
---
--- Índices para tabelas despejadas
---
-
---
--- Índices para tabela `users`
---
-ALTER TABLE `product`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `users`
---
-ALTER TABLE `product`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
